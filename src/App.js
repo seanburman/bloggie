873497a90e0ae0eb7c5a4c5dad8f391d7cef7e9a
Sean Burman <sean.burman@icloud.com>
2021-05-11 14:17:25 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import './assets/styles/grids.css'
+import './assets/styles/global-styles.css'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-app">
+      <div className="grid-container-main shadow">
+      <div className="container-create-account">
+        <img src="/img/logo.png" alt="Bloggie Logo" className="logo"/>
+        <h1>Create Account</h1>
+        <p>Start an account for free and have your own embeddable blog in minutes!</p>
+        <button className="login-signup shadow hover-bounce">Login or Signup</button>
+      </div>
+      <div className="grid-container-content bg-gradient">
+
+        <div className="content-items hover-bounce shadow">
+        <i className="fas fa-lightbulb" />
+        <p>Get Inspiration</p>
+        </div>
+        
+        <div className="content-items hover-bounce shadow">
+        <i className="fas fa-image" />
+        <p>Upload Images</p>
+        </div>
+        
+        <div className="content-items hover-bounce shadow">
+        <i className="fas fa-pencil-alt" />
+        <p>Write Posts</p>
+        </div>
+        
+        <div className="content-items hover-bounce shadow">
+        <i className="fas fa-cog" />
+        <p>Customize Themes</p>
+        </div>
+        
+        
+        
+        <div className="content-items hover-bounce shadow">
+        <i className="fas fa-code" />
+        <p>Embed Blog</p>
+        </div>
+        
+        <div className="content-items hover-bounce shadow">
+        <i className="fas fa-share" />
+        <p>Share Content</p>
+        </div>
+        
+        
+      </div>
+
+      </div>
     </div>
   );
 }
